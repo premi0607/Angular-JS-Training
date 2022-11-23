8f87e68d92038cfc9f6ab3ae9feb2781482525b0
premi0607 <53604720+premi0607@users.noreply.github.com>
2022-11-23 17:09:47 +0530

    Add files via upload

---
+++ Spring boot/department-service/src/main/java/net/javaguides/departmentservice/service/DepartmentService.java
@@ -0,0 +1,7 @@
+package net.javaguides.departmentservice.service;
+import net.javaguides.departmentservice.entity.Department;
+public interface DepartmentService {
+    Department saveDepartment(Department department);
+
+    Department getDepartmentById(Long departmentId);
+}
