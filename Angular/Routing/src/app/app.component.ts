a750646c1eef04533dc6db9098ff15136693f664
premi0607 <53604720+premi0607@users.noreply.github.com>
2022-11-23 17:32:49 +0530

    Add files via upload

---
+++ Angular/Routing/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'routing';
+
+}
+var name:string="marry";
+console.log("name"+name)
